aa5ceaeb8aa23165c5b121baccd93c0d9d9c324b
Vitor Curtolo <vitor.curtolo09@gmail.com>
2024-04-09 19:57:50 -0300

    Adicionando Responsividade

---
+++ script/script.js
@@ -0,0 +1,43 @@
+
+// const headerListMob = document.querySelector('.header-list-mob');
+// const header = document.querySelector('#header');
+
+// // Function to change the CSS properties
+// function adjustLayoutOn() {
+//     let headerListMob = document.querySelector(".header-list-mob");
+//     let header = document.getElementById("header");
+//     // Change .header-list-mob to display grid
+//     headerListMob.style.display = 'grid';
+//     // Change #header to display none
+//     header.style.display = 'none';
+// }
+
+// // Function to change the CSS properties
+// function adjustLayoutOff() {
+//     let headerListMob = document.querySelector(".header-list-mob");
+//     let header = document.getElementById("header");
+  
+//     // Change .header-list-mob to display grid
+//     headerListMob.style.display = 'none';
+//     // Change #header to display none
+//     header.style.display = 'flex';
+// }
+
+// window.addEventListener("resize", function() {
+//   const headerListMob = document.querySelector(".header-list-mob");
+//   const header = document.getElementById("header");
+
+//   if (window.innerWidth <= 730) {
+//     headerListMob.style.display = "grid";
+//     header.style.display = "none";
+//   } else {
+//     // Optional: Reset styles for larger screens (if needed)
+//     headerListMob.style.display = ""; // Inherit from CSS
+//     header.style.display = "";
+//   }
+// });
+
+
+
+
+
