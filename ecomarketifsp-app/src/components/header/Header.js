16dc12de1b5791fde5e64bceb8068e73a3cb178d
Vitor Curtolo <vitor.curtolo09@gmail.com>
2024-08-21 18:31:00 -0300

    aprimorando singIn e singOut

--- ecomarketifsp-app/src/components/header/Header.js
+++ ecomarketifsp-app/src/components/header/Header.js
@@ -2,40 +2,67 @@
 import {
     Link
 } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState, useEffect   } from 'react';
 import './Header.css';
 import '../../assets/styles/fonts.css';
 import logo from '../../assets/img/logoEcoMarket.png';
 import CadCliente from '../cadastroCliente/CadCliente';
+import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
 
 const Header = () => {
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  
-    const toggleMobileMenu = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+
+  const auth = getAuth();
+
+  useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+          if (user) {
+              setIsLogged(true);
+          } else {
+              setIsLogged(false);
+          }
+      });
+
+      // Cleanup subscription on unmount
+      return () => unsubscribe();
+  }, [auth]);
+
+  const toggleMobileMenu = () => {
       setIsMobileMenuOpen(!isMobileMenuOpen);
-    };
-  
-    return (
+  };
+
+  const signOutUser = () => {
+      signOut(auth).then(() => {
+        alert("Usuário desconectado!!")
+          console.log('Sign-out successful.');
+      }).catch((error) => {
+          console.error('An error happened:', error);
+      });
+  };
+
+  return (
       <header>
-        <div id="header">
-          <div className="header-logo">
-            <Link to="/"><img src={logo} alt="Logo" /></Link>
+          <div id="header">
+              <div className="header-logo">
+                  <Link to="/"><img src={logo} alt="Logo" /></Link>
+              </div>
+              <div className="header-list">
+                  <nav className="header-list-nav">
+                      <ul>
+                          <li><Link to="/home">Home</Link></li>
+                          {!isLogged && (<li><Link to="/login">Sign-in</Link></li>)}
+                          {isLogged && (<li><a onClick={signOutUser}>Sign-out</a></li>)}
+                          <li><Link to="/produtos">Cadastro Produto</Link></li>
+                          <li><Link to="/cadastro">Cadastro Cliente</Link></li>
+                          <li><Link to="/listar">Produtos</Link></li>
+                      </ul>
+                  </nav>
+              </div>
+              <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+                  &#9776; {/* Ícone de menu hamburguer */}
+              </div>
           </div>
-          <div className="header-list">
-            <nav className="header-list-nav">
-              <ul>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/login">Log-In</Link></li>
-                <li><Link to="/produtos">Cadastro Produto</Link></li>
-                <li><Link to="/cadastro">Cadastro Cliente</Link></li>
-                <li><Link to="/listar">Produtos</Link></li>
-              </ul>
-            </nav>
-          </div>
-          <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
-            &#9776; {/* Ícone de menu hamburguer */}
-          </div>
-        </div>
   
         {isMobileMenuOpen && (
           <div id="header-mob">
