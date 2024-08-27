55953bc0b7b81d8556a7f263d1fd04159db4a051
Vitor Curtolo <vitor.curtolo09@gmail.com>
2024-08-27 11:46:39 -0300

    fix: API CEP

--- ecomarketifsp-app/src/components/cadastroCliente/CadCliente.js
+++ ecomarketifsp-app/src/components/cadastroCliente/CadCliente.js
@@ -5,15 +5,13 @@ import { useAuth } from '../../context/AuthContext';
 import { collection, addDoc } from "firebase/firestore";
 import { db } from '../../assets/js/env'
 
-
 function CadCliente() {
-
-    //formatação de campo telefone
+    // Formatação de campo telefone
     const [phone, setPhone] = useState('');
     const [cep, setCep] = useState('');
     const { currentUser } = useAuth();
 
-    //dados cliente
+    // Dados cliente
     const [nome, setNome] = useState('');
     const [sobreNome, setSobreNome] = useState('');
     const [bairro, setBairro] = useState('');
@@ -24,7 +22,7 @@ function CadCliente() {
     const [complemento, setComple] = useState('');
 
     const handleInputChange = (event) => {
-        const input = event.target.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+        const input = event.target.value.replace(/\D/g, '');
         const formattedPhone = formatPhoneNumber(input);
         setPhone(formattedPhone);
     };
@@ -39,77 +37,55 @@ function CadCliente() {
         }
 
         if (phoneNumberLength < 8) {
-            return `(${value.slice(0, 2)})${value.slice(2)}`;
+            return `(${value.slice(0, 2)}) ${value.slice(2)}`;
         }
 
-        return `(${value.slice(0, 2)})${value.slice(2, 7)}-${value.slice(7, 11)}`;
+        return `(${value.slice(0, 2)}) ${value.slice(2, 7)}-${value.slice(7, 11)}`;
     };
 
-    function cleanFields() {
-        document.getElementById("bairro").value = "";
-        document.getElementById("cep").value = "";
-        document.getElementById("localidade").value = "";
-        document.getElementById("logradouro").value = "";
-        document.getElementById("uf").value = "";
-        document.getElementById("nome").value = "";
-        document.getElementById("sobreNome").value = "";
-        document.getElementById("phone").value = "";
-        document.getElementById("comple").value = "";
-        document.getElementById("numero").value = "";
-    }
+    const cleanFields = () => {
+        setBairro('');
+        setCep('');
+        setLocalidade('');
+        setLogradouro('');
+        setUf('');
+        setNome('');
+        setSobreNome('');
+        setPhone('');
+        setComple('');
+        setNumero('');
+    };
 
-    //consultar api de CEP
-
-    const [setJson] = useState({});
-
-
+    // Consultar API de CEP
     useEffect(() => {
-        const cepInput = document.getElementById("cep");
+        if (cep.length !== 8) return;
 
-        const handleBlur = async () => {
-            let Cep = cepInput.value;
-
-            if (!Cep.trim()) {
-                console.log("CEP está vazio");
-                cleanFields();
-                return;
-            }
-
-            let search = Cep.replace("-", "");
-
+        const fetchCepData = async () => {
             try {
-                const response = await fetch(`https://viacep.com.br/ws/${search}/json/`, {
-                    method: "GET",
-                    mode: "cors",
-                    caches: "default"
-                });
+                const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
 
                 if (!response.ok) {
-                    alert("Erro ao buscar CEP");
-                    console.error("Erro ao buscar o CEP");
-                    cleanFields();
-                    return;
+                    throw new Error("Erro ao buscar CEP");
                 }
 
                 const data = await response.json();
-                setJson(data);
-                document.getElementById("bairro").value = data.bairro || '';
-                document.getElementById("localidade").value = data.localidade || '';
-                document.getElementById("logradouro").value = data.logradouro || '';
-                document.getElementById("uf").value = data.uf || '';
+
+                if (data.erro) {
+                    throw new Error("CEP não encontrado");
+                }
+
+                setBairro(data.bairro || '');
+                setLocalidade(data.localidade || '');
+                setLogradouro(data.logradouro || '');
+                setUf(data.uf || '');
             } catch (error) {
-                alert("Erro ao buscar CEP");
-                console.error("Erro ao buscar o CEP:", error);
+                alert(error.message);
                 cleanFields();
             }
         };
 
-        cepInput.addEventListener("blur", handleBlur);
-
-        return () => {
-            cepInput.removeEventListener("blur", handleBlur);
-        };
-    }, []);
+        fetchCepData();
+    }, [cep]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -130,27 +106,21 @@ function CadCliente() {
             });
 
             alert("Cadastro realizado!!");
-            console.log('Cliente cadastrado com sucesso!');
             cleanFields();
-
         } catch (error) {
             alert("Erro ao cadastrar !");
-            console.error('Erro ao cadastrar cliente:', error);
             cleanFields();
         }
     };
 
     return (
         <main className="mainCadCliente">
-
             <div className="form-main">
                 <div className="form-card">
                     <form onSubmit={handleSubmit}>
                         <div className="form-title">
                             <h2 className="">Cadastro de Clientes</h2>
-                            <p>
-                                Cadastre-se e fique por dentro das mais novas atualizações e promoções!
-                            </p>
+                            <p>Cadastre-se e fique por dentro das mais novas atualizações e promoções!</p>
                         </div>
 
                         <div className="form-input-flex">
@@ -158,35 +128,34 @@ function CadCliente() {
                                 <label htmlFor="nome" className="form-label">Nome</label>
                                 <input
                                     type="text"
-                                    name="nome"
                                     id="nome"
                                     className="form-input"
                                     value={nome}
                                     onChange={(e) => setNome(e.target.value)}
-                                    required />
+                                    required
+                                />
                             </div>
 
                             <div>
-                                <label htmlFor="sobreNome" className="form-label"> Sobrenome </label>
+                                <label htmlFor="sobreNome" className="form-label">Sobrenome</label>
                                 <input
                                     type="text"
-                                    name="sobreNome"
                                     id="sobreNome"
+                                    className="form-input"
                                     value={sobreNome}
                                     onChange={(e) => setSobreNome(e.target.value)}
-                                    className="form-input"
-                                    required />
+                                    required
+                                />
                             </div>
                         </div>
 
                         <div className="form-input-flex">
                             <div>
-                                <label htmlFor="phone" className="form-label"> Telefone </label>
+                                <label htmlFor="phone" className="form-label">Telefone</label>
                                 <input
-                                    className='form-input'
+                                    className="form-input"
                                     type="text"
                                     id="phone"
-                                    name="phone"
                                     value={phone}
                                     onChange={handleInputChange}
                                     placeholder="Digite seu telefone"
@@ -195,89 +164,86 @@ function CadCliente() {
                                 />
                             </div>
                             <div>
-                                <label htmlFor="cep" className="form-label"> CEP </label>
+                                <label htmlFor="cep" className="form-label">CEP</label>
                                 <input
                                     type="text"
-                                    name="cep"
                                     id="cep"
                                     maxLength="9"
                                     className="form-input"
                                     value={cep}
-                                    onChange={(e) => setCep(e.target.value)}
+                                    onChange={(e) => setCep(e.target.value.includes("-") ? e.target.value.replace("-", "") : e.target.value)}
                                     required
                                 />
                             </div>
-
                         </div>
 
                         <div className="form-input-flex">
                             <div>
-                                <label htmlFor="bairro" className="form-label"> Bairro </label>
+                                <label htmlFor="bairro" className="form-label">Bairro</label>
                                 <input
                                     type="text"
-                                    name="bairro"
                                     id="bairro"
                                     className="form-input"
                                     value={bairro}
                                     onChange={(e) => setBairro(e.target.value)}
-                                    required />
+                                    required
+                                />
                             </div>
 
                             <div>
                                 <label htmlFor="logradouro" className="form-label">Endereço</label>
                                 <input
                                     type="text"
-                                    name="logradouro"
                                     id="logradouro"
                                     className="form-input"
                                     value={endereco}
                                     onChange={(e) => setLogradouro(e.target.value)}
-                                    required />
+                                    required
+                                />
                             </div>
                         </div>
 
                         <div className="form-input-flex">
                             <div>
-                                <label htmlFor="localidade" className="form-label"> Cidade </label>
+                                <label htmlFor="localidade" className="form-label">Cidade</label>
                                 <input
                                     type="text"
-                                    name="localidade"
                                     id="localidade"
                                     className="form-input"
                                     value={cidade}
                                     onChange={(e) => setLocalidade(e.target.value)}
-                                    required />
+                                    required
+                                />
                             </div>
                             <div>
-                                <label htmlFor="uf" className="form-label"> Estado </label>
+                                <label htmlFor="uf" className="form-label">Estado</label>
                                 <input
                                     type="text"
-                                    name="uf"
                                     id="uf"
                                     className="form-input"
                                     value={uf}
                                     onChange={(e) => setUf(e.target.value)}
-                                    required />
+                                    required
+                                />
                             </div>
                         </div>
 
                         <div className="form-input-flex">
                             <div>
-                                <label htmlFor="numero" className="form-label"> Número </label>
+                                <label htmlFor="numero" className="form-label">Número</label>
                                 <input
                                     type="text"
-                                    name="numero"
                                     id="numero"
                                     className="form-input"
                                     value={numero}
                                     onChange={(e) => setNumero(e.target.value)}
-                                    required />
+                                    required
+                                />
                             </div>
                             <div>
-                                <label htmlFor="comple" className="form-label"> Complemento </label>
+                                <label htmlFor="comple" className="form-label">Complemento</label>
                                 <input
                                     type="text"
-                                    name="comple"
                                     id="comple"
                                     className="form-input"
                                     value={complemento}
@@ -287,11 +253,9 @@ function CadCliente() {
                         </div>
 
                         <button className="formbold-btn" type="submit">Cadastrar</button>
-
                     </form>
                 </div>
             </div>
-
         </main>
     );
 }
